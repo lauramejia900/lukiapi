1ac4cfef06d1a012b2394ebc76b250e6a65982c5
Laura Mejía M <93622576+lauramejia900@users.noreply.github.com>
2022-10-27 19:52:40 -0500

    Luke APIwalker

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import ComponeteApi from './MisComponentes/ComponenteApi/ComponenteApi';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import ComponenteRuta from './ComponenteRuta/ComponenteRuta';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Switch>
+      <Route path="/" exact render={() => <ComponeteApi></ComponeteApi>}/>
+      <Route path="/:id" exact render={(routeProps) => <ComponenteRuta {...routeProps}></ComponenteRuta>}/>
+    </Switch>
+    </BrowserRouter>
+    
   );
 }
 
